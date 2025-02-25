f0a0528e0f9d7a982aa647b44f1c933754aa708d
wot <axel@wot.id>
2025-02-25 11:06:14 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,8 @@ import './globals.css';
 import ContextProvider from '@/context'
 
 export const metadata: Metadata = {
-  title: "AppKit in Next.js + wagmi",
-  description: "AppKit example dApp",
+  title: "wot.id",
+  description: "Trusted Identity",
 };
 
 export default async function RootLayout({
