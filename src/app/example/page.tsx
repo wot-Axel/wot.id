d89f3cc277b720469970ace55731a25be9374c12
wot-Axel <axel@wot.id>
2025-03-01 11:52:24 +0000

    13

---
+++ src/app/example/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Example</h1>
+      <p>
+        This is an example page. You can find the source code for this page in <code>src/app/example/page.tsx</code>.
+      </p>
+    </div>
+  )
+}
