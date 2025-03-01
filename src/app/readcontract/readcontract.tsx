99e5a5ef6a554e07361d2c50947cb999e70c6790
wot-Axel <axel@wot.id>
2025-03-01 12:53:38 +0000

    Update readcontract.tsx

--- src/app/readcontract/readcontract.tsx
+++ src/app/readcontract/readcontract.tsx
@@ -7,7 +7,7 @@ export default function ReadContract() {
   const { data: balance } = useReadContract({
     ...wagmiContractConfig,
     functionName: 'balanceOf',
-    args: ['0xBBfB973B887DD339eC01E3335be71415e0f1D41b'],
+    args: ['0x7D78710570D65b17D860Dd6AC51ECa426cc8Ee9B'],
   })
 
   return (
