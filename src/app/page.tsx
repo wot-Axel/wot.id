a5833b3cb7913db32f87f2dc476463ba047f8ad4
wot-Axel <axel@wot.id>
2025-03-03 11:46:22 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,8 @@ export default function Home() {
           This projectId only works on localhost. <br/>Go to <a href="https://cloud.reown.com" target="_blank" className="link-button" rel="Reown Cloud">Reown Cloud</a> to get your own.
         </p>
       </div>
+      <br />
+      <br />
       <Footer />
     </div>
   );
