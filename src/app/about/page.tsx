38bc9c7c2f1f97e335a8ccaff9e308b79346e2ac
wot-Axel <axel@wot.id>
2025-03-03 10:50:35 +0000

    31

---
+++ src/app/about/page.tsx
@@ -0,0 +1,23 @@
+//import { TopNav } from "@/components/TopNav";
+import { Footer } from "@/components/Footer";
+
+const About = () => {
+  return (
+    <div>
+    
+      <h1>About</h1>
+      <h2>Caution</h2>
+      <br />
+      <p>This dApp is experimental and not to be used without the necessary thorough knowledge of blockchain technology.</p>
+      <br />
+      <p>
+      Any use is with a full understanding of and consent to the conditions provided in the disclaimer.
+      </p>
+      <br />
+      <br />
+    <Footer />
+    </div>
+  );
+}
+
+export default About;
