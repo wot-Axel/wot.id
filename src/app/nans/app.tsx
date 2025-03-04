20ec695e3793344a81fa45ef39a7fcb56d3389e5
wot-Axel <axel@wot.id>
2025-03-04 10:31:26 +0000

    41

---
+++ src/app/nans/app.tsx
@@ -0,0 +1,140 @@
+import { SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
+import { useState } from "react";
+import { SCHEMA } from "./config";
+import { useEAS } from "./useEAS";
+import { Address, ethAddress } from "viem";
+
+type AttestationData = {
+  wotid: Address;
+  ishuman: boolean;
+};
+/** @dev AFTER REGISTERING A SCHEMA, OR MAKING AN ATTESTATION
+ * IF YOU REFRESH APP MAKE SURE TO PASTE IN SCHEMA/ATTESTATIONUID IN STATE VARIABLES OR ELSE APP WONT WORK
+ * */
+const App = () => {
+  const { eas, currentAddress } = useEAS();
+  console.log("currentAddress ", currentAddress);
+  // schemaUID is set when Freelancer register's their own reputation schema
+  const [schemaUID] = useState<string>(
+    "0xfda16985b01f97d81468a76dee939af365d518910ed2ebf06400290aff490fcf"
+  );
+  const [attestationUID, setAttestationUID] = useState<string>("");
+  const [attestationData, setAttestationData] = useState<AttestationData>({
+    wotid: ethAddress,
+    ishuman: false,
+  });
+
+  const handleAttestationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, type, value, checked } = e.target;
+
+    setAttestationData({
+      ...attestationData,
+      [name]: type === "checkbox" ? checked : value,
+    });
+  };
+
+  const createAttestation = async () => {
+    if (!eas || !schemaUID) return;
+    const schemaEncoder = new SchemaEncoder(SCHEMA);
+    const encodedData = schemaEncoder.encodeData([
+      { name: "", value: currentAddress, type: "string" },
+      { name: "ishuman", value: attestationData.ishuman, type: "bool" },
+    ]);
+
+    const transaction = await eas.attest({
+      schema: schemaUID,
+      data: {
+        recipient: attestationData.wotid,
+        expirationTime: undefined,
+        revocable: true, // Be aware that if your schema is not revocable, this MUST be false
+        data: encodedData,
+      },
+    });
+
+    const newAttestationUID = await transaction.wait();
+    setAttestationUID(newAttestationUID);
+
+    console.log("New attestation UID:", newAttestationUID);
+    console.log("Creating Attestation:", attestationData);
+  };
+
+
+
+    const transaction = await eas.attest({
+      schema: schemaUID,
+      data: {
+        recipient: attestationData.wotid,
+        expirationTime: undefined,
+        revocable: true, // Be aware that if your schema is not revocable, this MUST be false
+        data: encodedData,
+      },
+    });
+
+    const newAttestationUID = await transaction.wait();
+    setAttestationUID(newAttestationUID);
+
+    console.log("New attestation UID:", newAttestationUID);
+    console.log("Creating Attestation:", attestationData);
+  };
+
+  const revokeAttestation = async () => {
+    if (!eas) return;
+    const attestation = await eas.getAttestation(attestationUID);
+
+    const transaction = await eas.revoke({
+      schema: attestation.schema,
+      data: { uid: attestation.uid },
+    });
+    const receipt = await transaction.wait();
+    console.log("Revoking Attestation:", receipt);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <h1>Ethereum Attestation Service</h1>
+      <h2 style={{ textAlign: "center" }}>
+        {!schemaUID
+          ? "Step 1: Freelancer registers a schema for their own reputation"
+          : "Step 2: Client creates attestation for Freelancer`s credibility"}
+      </h2>
+
+      
+
+      {schemaUID && (
+        <>
+          <h2>Create Attestation</h2>
+          <input
+            type="address"
+            name="wotid"
+            value={attestationData.wotid}
+            onChange={handleAttestationChange}
+            placeholder="wotid"
+          />
+          
+          <label htmlFor="ishuman">
+            This address represents a human being
+          </label>
+          <input
+            type="checkbox"
+            id="ishuman"
+            name="ishuman"
+            checked={attestationData.ishuman}
+            onChange={handleAttestationChange}
+          />
+          <button onClick={createAttestation}>Create Attestation</button>
+
+          <h2>Revoke Attestation</h2>
+          <button onClick={revokeAttestation}>Revoke Attestation</button>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/app/nans/useEAS.ts
@@ -0,0 +1,38 @@
+import { EAS, SchemaRegistry } from "@ethereum-attestation-service/eas-sdk";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { EAS_ADDRESS, SCHEMA_REGISTRY_ADDRESS } from "./config";
+
+export const useEAS = () => {
+  const [eas, setEAS] = useState<EAS>();
+  const [schemaRegistry, setSchemaRegistry] = useState<SchemaRegistry>();
+  const [currentAddress, setCurrentAddress] = useState("");
+  useEffect(() => {
+    if (currentAddress) return;
+    const init = async () => {
+      // Initialize the sdk with the address of the EAS Schema contract address
+      const easInstance = new EAS(EAS_ADDRESS);
+      const schemaRegistry = new SchemaRegistry(SCHEMA_REGISTRY_ADDRESS);
+
+      // Gets a default provider (in production use something else like infura/alchemy)
+      if (!window.ethereum) {
+        throw new Error("Ethereum provider is not available");
+      }
+      const provider = new ethers.BrowserProvider(window.ethereum as ethers.providers.ExternalProvider);
+      const signer = await provider.getSigner();
+      const address = await signer.getAddress();
+
+      // Connects an ethers style provider/signingProvider to perform read/write functions.
+      easInstance.connect(signer); // allow clients to attest against freelancer's schema
+      schemaRegistry.connect(signer); // allow Freelancer to register their own reputation schema
+      setEAS(easInstance);
+      setSchemaRegistry(schemaRegistry);
+      setCurrentAddress(address);
+    };
+    init();
+
+    // setCurrentAddress(provider);
+  }, [eas, schemaRegistry, currentAddress]);
+
+  return { eas, schemaRegistry, currentAddress };
+};
