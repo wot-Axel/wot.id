710730203ddedefd3146fc5474b842784117a6f1
wot <axel@wot.id>
2025-03-09 12:27:18 +0000

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
-
-
-import { headers } from 'next/headers' // added
+import { headers } from 'next/headers';
 import './globals.css';
-import ContextProvider from '@/context'
+import ContextProvider from '@/context';
+import { TopNavigation } from '@/components/TopNavigation';
 
 export const metadata: Metadata = {
   title: "wot.id",
@@ -21,7 +20,12 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ContextProvider cookies={cookies}>{children}</ContextProvider>
+        <ContextProvider cookies={cookies}>
+          <TopNavigation />
+          <main className="main-content">
+            {children}
+          </main>
+        </ContextProvider>
       </body>
     </html>
   );
