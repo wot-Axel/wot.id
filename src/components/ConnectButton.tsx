718b5e0b9edf6956dd9c3e6320cb8bb5f7c63b1e
wot-Axel <axel@wot.id>
2025-03-21 15:12:56 +0100

    fix: center connect wallet button on home page

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -2,7 +2,7 @@
 
 export const ConnectButton = () => {
   return (
-    <div >
+    <div style={{ display: 'flex', justifyContent: 'center', width: '100%', margin: '1rem 0' }}>
         {/* @ts-expect-error Add this line while our team fix the upgrade to react 19 for global components */}
         <appkit-button />
     </div>
