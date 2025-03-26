0c8ac68c33894ecbfc941a5f9be8b6d57b73a1ad
wot-Axel <axel@wot.id>
2025-03-26 12:18:30 +0100

    Change 'Connect Wallet' button text to 'Connect' and improve button styling

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,10 +1,25 @@
 'use client'
 
+import { useAppKit } from '@reown/appkit/react'
+import { useAppKitAccount } from '@reown/appkit/react'
+
 export const ConnectButton = () => {
+  const { open } = useAppKit()
+  const { isConnected } = useAppKitAccount()
+
   return (
     <div style={{ display: 'flex', justifyContent: 'center', width: '100%', margin: '1rem 0' }}>
-        {/* @ts-expect-error Add this line while our team fix the upgrade to react 19 for global components */}
+      {!isConnected ? (
+        <button 
+          onClick={() => open()}
+          className="connect-button"
+        >
+          Connect
+        </button>
+      ) : (
+        // @ts-expect-error Add this line while our team fix the upgrade to react 19 for global components
         <appkit-button />
+      )}
     </div>
   )
 }
