77fd49b023ffc3311a7bedc2d13bc88d8f35ddbd
wot-Axel <axel@wot.id>
2025-03-28 13:00:56 +0100

    Fix width issues on iPhone for me page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Footer } from '@/components/Footer';
 export const metadata: Metadata = {
   title: "wot.id",
   description: "Trusted Identity",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default async function RootLayout({
